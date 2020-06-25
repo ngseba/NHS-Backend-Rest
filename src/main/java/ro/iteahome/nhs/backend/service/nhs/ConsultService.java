6dc336f7dd4deb4f8ddc1b2ce5d359f4ce46df53
Marius Bistika <marius.bistika@gmail.com>
2020-06-25 22:11:16 +0300

    working on consult

--- src/main/java/ro/iteahome/nhs/backend/service/nhs/ConsultService.java
+++ src/main/java/ro/iteahome/nhs/backend/service/nhs/ConsultService.java
@@ -1,7 +1,12 @@
 package ro.iteahome.nhs.backend.service.nhs;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.hateoas.EntityModel;
 import org.springframework.stereotype.Service;
+import ro.iteahome.nhs.backend.model.nhs.dto.ConsultDTO;
+import ro.iteahome.nhs.backend.model.nhs.entity.Consult;
+import ro.iteahome.nhs.backend.repository.nhs.DiagnosticRepository;
+import ro.iteahome.nhs.backend.repository.nhs.InstitutionRepository;
 import ro.iteahome.nhs.backend.repository.nhs.PatientRepository;
 
 @Service
@@ -9,8 +14,12 @@ public class ConsultService {
 
     @Autowired
     private PatientRepository patientRepository;
-//    @Autowired
-//    private
+    @Autowired
+    private InstitutionRepository institutionRepository;
+    @Autowired
+    private DiagnosticRepository diagnosticRepository;
 
-
+//    public EntityModel<ConsultDTO> add(Consult consult) {
+////        consult
+//    }
 }
