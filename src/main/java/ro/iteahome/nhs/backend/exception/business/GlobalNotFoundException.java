065e4c11264dcc69601c80caf233b61849818f02
Mihai Szocs <szocsmihai@gmail.com>
2020-07-04 00:30:41 +0300

    Working on exceptions.

--- src/main/java/ro/iteahome/nhs/backend/exception/business/GlobalNotFoundException.java
+++ src/main/java/ro/iteahome/nhs/backend/exception/business/GlobalNotFoundException.java
@@ -5,7 +5,7 @@ public class GlobalNotFoundException extends RuntimeException {
     private final String entityName;
 
     public GlobalNotFoundException(String entityName) {
-        super(entityName + " NOT FOUND.");
+        super(entityName + " NOT FOUND");
         this.entityName = entityName;
     }
 
