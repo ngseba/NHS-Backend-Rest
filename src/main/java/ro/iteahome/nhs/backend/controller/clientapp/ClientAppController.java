03d6fd0b7c33d303433b2e054d205170aaa2e51d
Mihai Szocs <szocsmihai@gmail.com>
2020-07-07 14:35:25 +0300

    Working on exceptions.

--- src/main/java/ro/iteahome/nhs/backend/controller/clientapp/ClientAppController.java
+++ src/main/java/ro/iteahome/nhs/backend/controller/clientapp/ClientAppController.java
@@ -3,11 +3,7 @@ package ro.iteahome.nhs.backend.controller.clientapp;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.hateoas.EntityModel;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.validation.FieldError;
-import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.*;
 import ro.iteahome.nhs.backend.model.clientapp.dto.ClientAppCredentials;
 import ro.iteahome.nhs.backend.model.clientapp.dto.ClientAppDTO;
@@ -16,9 +12,6 @@ import ro.iteahome.nhs.backend.service.clientapp.ClientAppService;
 import ro.iteahome.nhs.backend.service.clientapp.RoleService;
 
 import javax.validation.Valid;
-import java.util.LinkedHashMap;
-import java.util.Map;
-import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/client-apps")
@@ -77,23 +70,5 @@ public class ClientAppController {
     @PreAuthorize("hasRole('ADMIN')")
     public EntityModel<ClientAppDTO> deleteByName(@PathVariable String name) {
         return clientAppService.deleteByName(name);
-    }
-
-// OTHER METHODS: ------------------------------------------------------------------------------------------------------
-
-    @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity<Map<String, String>> handleMethodArgumentNotValid(MethodArgumentNotValidException ex) {
-        Map<String, String> errors = new LinkedHashMap<>();
-        errors.put("errorCode", "CLI-00");
-        errors.put("errorMessage", "CLIENT APP FIELDS HAVE VALIDATION ERRORS.");
-        errors.putAll(ex.getBindingResult()
-                .getFieldErrors()
-                .stream()
-                .collect(Collectors.toMap(
-                        FieldError::getField,
-                        FieldError::getDefaultMessage)));
-        return new ResponseEntity<>(
-                errors,
-                HttpStatus.BAD_REQUEST);
     }
 }
